bef1089b7a24970ad06703213e5010eb68fe1a60
Renata Hassum <renatahassum@gmail.com>
2023-06-27 16:49:27 -0300

    Send email

--- integrations/src/main/java/com/devsuperior/integrations/controllers/EmailController.java
+++ integrations/src/main/java/com/devsuperior/integrations/controllers/EmailController.java
@@ -1,6 +1,8 @@
 package com.devsuperior.integrations.controllers;
 
 import com.devsuperior.integrations.dto.EmailDTO;
+import com.devsuperior.integrations.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailController {
+
+    @Autowired
+    EmailService service;
+
     @PostMapping
     public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+        service.sendEmail(dto);
         return ResponseEntity.noContent().build();
     }
+
+
 }
