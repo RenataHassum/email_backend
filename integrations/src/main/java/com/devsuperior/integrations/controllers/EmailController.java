5adf4dab60d59d65d348256d2df4e1ac0685b397
Renata Hassum <renatahassum@gmail.com>
2023-06-27 16:30:33 -0300

    Project created, post, dto

---
+++ integrations/src/main/java/com/devsuperior/integrations/controllers/EmailController.java
@@ -0,0 +1,17 @@
+package com.devsuperior.integrations.controllers;
+
+import com.devsuperior.integrations.dto.EmailDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/emails")
+public class EmailController {
+    @PostMapping
+    public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+        return ResponseEntity.noContent().build();
+    }
+}
